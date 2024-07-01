4cbe0c917993e83090718fcc4133bbae926402ef
developer-sujon <devoloper.sujon@gmail.com>
2024-07-01 16:55:01 +0600

    Invoice Generator

---
+++ invoice-generator/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuBar from "./components/MenuBar";
+import InvoiceGeneratorPage from "./pages/InvoiceGeneratorPage";
+
+const App = () => {
+  return (
+    <>
+      <MenuBar />
+      <InvoiceGeneratorPage />
+    </>
+  );
+};
+
+export default App;
---
+++ invoice-generator/src/pages/InvoiceGeneratorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import InvoiceForm from "../components/InvoiceForm";
+
+const InvoiceGeneratorPage = () => {
+  return (
+    <Container>
+      <InvoiceForm />
+    </Container>
+  );
+};
+
+export default InvoiceGeneratorPage;
